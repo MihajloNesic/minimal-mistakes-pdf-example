2729882922a34d3bde4d5ea8270878f40899106e
MihajloNesic <MihajloNesic@users.noreply.github.com>
2022-01-29 17:37:55 +0000

    jekyll build from Action 7ec1c34f984d2834d4455dd2be79e61f4588a4d4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/minimal-mistakes-pdf-example/welcome-to-jekyll",
+        "teaser": null
+      },{
+        "title": "jekyll-pdf-embed test",
+        "excerpt":" {% pdf \"https://mihajlonesic.gitlab.io/files/loremipsum.pdf\" %}     View PDF         Base URL must be present if baseurl is set in _config.yml. In this example, minimal-mistakes-pdf-example is prepended to file location /files/dummy.pdf.   {% pdf \"/minimal-mistakes-pdf-example/files/dummy.pdf\" no_link height=300px %}        ","categories": ["jekyll","plugin"],
+        "tags": [],
+        "url": "/minimal-mistakes-pdf-example/jekyll-pdf-embed-test",
+        "teaser": null
+      }]
